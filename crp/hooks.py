6140ae8095ca49f96b8c3c9a1f35082e06ca5759
Lesia Chernysh <91280391+Lesia-Chernysh@users.noreply.github.com>
2026-09-12 19:39:48 +0200

    Remove debug print from backward method
    Removed print statement from backward method.

--- crp/hooks.py
+++ crp/hooks.py
@@ -101,7 +101,6 @@ class FeatVisHook:
 
     def backward(self, module, grad):
         '''Hook applied during backward-pass'''
-        print(f"hooks/backward. dict_inputs: {self.dict_inputs.keys()}")
 
         s_indices, targets, additional_forward_kwargs = self.dict_inputs["sample_indices"], self.dict_inputs["targets"], self.dict_inputs["additional_forward_kwargs"]
         relevance = grad.detach().to(self.on_device) if self.on_device else grad.detach()
